2599fd2ad6b19d99c4c59258c977ba7c54baa476
lamdevhs <lam.dev.hs@gmail.com>
2019-04-09 19:01:10 +0200

    Add pretty printer for mlexp (V1)

--- miniml.ml
+++ miniml.ml
@@ -17,7 +17,6 @@ type primop =
   UnOp of unop
 | BinOp of binop
 
-
 type mlexp =
   Var of var
 | Bool of bool
@@ -34,3 +33,45 @@ type prog = Prog of typedef option * mlexp
 
 let mlexp_of_prog (Prog(t, e)) = e
 
+
+(* -------------------- pretty printer: ---------------- *)
+
+let pp_unop : unop ->  string = function
+  Fst -> "first" | Snd -> "second"
+;;
+
+let pp_barith : barith -> string = function
+  BAadd -> "+" | BAsub -> "-" | BAmul -> "*" | BAdiv -> "/" | BAmod -> "mod"
+;;
+
+
+let pp_bcompar : bcompar ->  string = function
+  BCeq -> "=" | BCge -> ">=" | BCgt -> ">" | BCle -> "<=" | BClt -> "<" | BCne -> "<>"
+;;
+
+let pp_binop : binop ->  string = function
+    BArith b -> pp_barith b
+  | BCompar b -> pp_bcompar b
+;;
+
+
+let pp_primop : primop ->  string = function
+  | UnOp x -> pp_unop x
+  | BinOp x -> pp_binop x
+;;
+let rec pp_exp : mlexp -> string = function
+  | Var v -> v
+  | Bool b -> string_of_bool b
+  | Int i -> string_of_int i
+  | PrimOp po -> pp_primop po
+  | Cond (i, t, e) -> "if " ^ pp_exp i ^ " then " ^ pp_exp t ^ " else " ^ pp_exp e
+  | Pair (x, y) -> "(" ^ pp_exp x ^ "," ^ pp_exp y ^ ")"
+  | App (PrimOp op, Pair (x, y)) -> "(" ^ pp_exp x ^ " " ^ pp_primop op ^ " " ^ pp_exp y ^ ")"
+  | App (f, x) -> "(" ^ pp_exp f ^ " " ^ pp_exp x ^ ")"
+  | Fn (v, b) -> "(fun " ^ v ^ " -> " ^ pp_exp b ^ ")"
+  | Fix _ -> "fix"
+;;
+
+let pp : prog -> string = function
+  Prog (_, exp) -> pp_exp exp
+;;
